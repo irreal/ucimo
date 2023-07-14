4172b4f67cfa8732571cf447343cdd1c3b7dedd0
Milos Spasojevic <milos.s.pfc@gmail.com>
2023-07-14 12:14:57 +0200

    test b

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} jeeeej
+          count is {count} jeeeej ovo je test branch
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
